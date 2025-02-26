0e2eaf21e7a0e4fbad89b5629e9ce357beaa59dc
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-02-25 23:16:28 -0300

    Update Vip.java

--- ingresso/Vip.java
+++ ingresso/Vip.java
@@ -1,26 +1,21 @@
 package ingresso;
 
 public class Vip extends Ingresso{
-    private double ingressoVip;
-    public Vip(){
-        super();
-    }
-    public Vip(double valor){
-        super(valor);
-        this.ingressoVip=ingressoVip;
+    private double adicional=20;
+
+    public Vip() { }
+
+    public double ValorVIP() {
+        return getValor() + getAdicional();
     }
 
     @Override
     public void imprimirValor() {
-        super.imprimirValor();
-        System.out.println("Ingresso VIP");
+        System.out.println("Ingresso VIP: R$"+ValorVIP());
     }
 
-    public double getIngressoVip() {
-        return ingressoVip;
-    }
 
-    public void setIngressoVip(double ingressoVip) {
-        this.ingressoVip = ingressoVip;
+    public double getAdicional() {
+        return adicional;
     }
 }
