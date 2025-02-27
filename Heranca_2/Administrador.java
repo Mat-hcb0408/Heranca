e281d5841b1298f628e4d9c8ca4ec2b28b7c0881
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-02-27 16:57:16 -0300

    Add files via upload

---
+++ Heranca_2/Administrador.java
@@ -0,0 +1,29 @@
+package Heranca_2;
+
+public class Administrador extends Empregado{
+    private double ajudaDeCusto;
+
+    public Administrador(String nome, String telefone, String endereco, int codigoSetor, double salarioBase, double imposto, double ajudaDeCusto) {
+        super(nome, telefone, endereco, codigoSetor, salarioBase, imposto);
+        this.ajudaDeCusto = ajudaDeCusto;
+    }
+
+    public double getAjudaDeCusto() {
+        return ajudaDeCusto;
+    }
+
+    public void setAjudaDeCusto(double ajudaDeCusto) {
+        this.ajudaDeCusto = ajudaDeCusto;
+    }
+
+    public double calcularSalario(){
+        return super.calcularSalario()-(getSalarioBase()*getAjudaDeCusto()/100);
+    }
+
+    @Override
+    public void exibirPessoa() {
+        super.exibirPessoa();
+        System.out.printf("Ajuda de custo: $%.2f%%%n" +
+                "Salario liquido: $%.2f%n",getAjudaDeCusto(),calcularSalario());
+    }
+}
---
+++ Heranca_2/Empregado.java
@@ -0,0 +1,54 @@
+package Heranca_2;
+
+public class Empregado extends Pessoa{
+    private int codigoSetor;
+    private double salarioBase;
+    private  double imposto;
+
+
+    public Empregado(String nome, String telefone, String endereco, int codigoSetor, double salarioBase, double imposto) {
+        super(nome, telefone, endereco);
+        this.codigoSetor = codigoSetor;
+        this.salarioBase = salarioBase;
+        this.imposto = imposto;
+    }
+
+
+
+    public int getCodigoSetor() {
+        return codigoSetor;
+    }
+
+    public void setCodigoSetor(int codigoSetor) {
+        this.codigoSetor = codigoSetor;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public double getImposto() {
+        return imposto;
+    }
+
+    public void setImposto(double imposto) {
+        this.imposto = imposto;
+    }
+
+    public double calcularSalario(){
+        return getSalarioBase()-(getSalarioBase()*getImposto()/100);
+    }
+
+    @Override
+    public void exibirPessoa() {
+        super.exibirPessoa();
+        System.out.printf("Código do setor: %d%n" +
+                "Salario base: %.2f%n" +
+                "Imposto: %.2f%%%n" +
+                "Salario liquido: %.2f%n",getCodigoSetor(),getSalarioBase(),getImposto(),calcularSalario());
+    }
+}
